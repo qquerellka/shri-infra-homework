b293de81ed816cec6a6231600b063f31483e25df
qquerellka <uskovmaksof@gmail.com>
2025-07-11 13:16:34 +0300

    Update About.tsx

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: [Your Name]
+            Author: Uskov Maksim
           </p>
         </div>
       </div>
